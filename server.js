d38500ba26aa326a752d2fb090f3b45fa751662c
Dinis Cruz <dinis.cruz@owasp.org>
2016-05-01 18:29:11 +0100

    adding support for cloud trace

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 require('coffee-script/register');
+require('@google/cloud-trace').start();
+
+console.log('Staring bsimm-graph server');
 var D3_Server = require('./src/D3-Server');
 var d3_server = new D3_Server();
 d3_server.run();
--- start-server.coffee
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env coffee
-require 'fluentnode'
-D3_Server = require './src/D3-Server'
-
-using new D3_Server(), ->
-  @.run()
-  console.log "Server started on #{@.server_Url()}"
