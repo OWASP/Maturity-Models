040305ff0f5aeda3a8420ed61e61d7bac0abd089
Dinis Cruz <dinis.cruz@owasp.org>
2016-05-18 13:41:31 +0100

    updated data ref Added docker file that builds the entire image

--- deploy/build.sh
+++ deploy/build.sh
@@ -5,7 +5,13 @@ echo "Creating Docker image for BSIMM-Graph"
 docker build -t bsimm-graph ./docker-images/1.base-image/.
 docker build -t bsimm-graph ./docker-images/2.git-pull/.
 
-#docker rm bsimm-web
-#docker run --name bsimm-web -it -p 3000:3000 bsimm-graph
+echo "Stopping bsimm-web container"
+docker stop bsimm-web
+docker rm bsimm-web
+docker run --name bsimm-web -it -p 3000:3000 -d bsimm-graph
+
+#open http://192.168.99.100:3000
+
+docker ps
 
 
