e29c98e08c8ac65727c79b34e6420f7cc293eca9
Dinis Cruz <dinis.cruz@owasp.org>
2016-07-11 11:41:26 +0100

    Removing ui submodule (it will be added inside the code folder)

--- bin/local-code-coverage.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mocha --recursive --compilers coffee:coffee-script/register --require coffee-coverage/register-istanbul test
-./node_modules/.bin/istanbul report
-open ./coverage/lcov-report/index.html
